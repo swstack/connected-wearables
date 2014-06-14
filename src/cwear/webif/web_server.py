ac24f5cd61e8c5d3570a6e1a4049e5fe4d586dc9
Tom Manley <tom.manley@etherios.com>
2014-06-14 00:12:24 -0500

    [webif] enabled debugging

--- src/cwear/webif/web_server.py
+++ src/cwear/webif/web_server.py
@@ -5,6 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 HAPI_CLIENT_ID = os.environ.get('HAPI_CLIENT_ID')
 
 app = Flask(__name__, static_url_path="")
+app.debug = True
 app.secret_key = "1234"
 db = SQLAlchemy(app)
 
