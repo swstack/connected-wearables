86d6b0371995d3cae5087abc60d360b83f4ba129
Tom Manley <tom.manley@etherios.com>
2014-06-13 21:02:24 -0500

    [webif] Created "hello world" web interface

---
+++ src/cwear/webif/web_server.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path="")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
