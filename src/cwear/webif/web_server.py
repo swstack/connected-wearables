1be2a7ee43c1a46754a352408b2e88e043963dfa
Tom Manley <tom.manley@etherios.com>
2014-06-13 22:07:00 -0500

    [webif] Basic HumanAPI login and dashboard template

--- src/cwear/webif/web_server.py
+++ src/cwear/webif/web_server.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="")
 
+HAPI_CLIENT_ID = os.environ.get('HAPI_CLIENT_ID')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', hapi_client_id=HAPI_CLIENT_ID)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
