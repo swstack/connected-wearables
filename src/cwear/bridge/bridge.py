3491d47caf34a5f02b669c81d11ef8a1bd70f887
Paul Osborne <paul.osborne@etherios.com>
2014-06-14 13:18:45 -0500

    [core] Describe additional batch endpoints and better batching to cloud
    This change moves the declaration of structures for each of the fields
    we map from the human API to the device cloud to a yaml file as the syntax
    is a bit cleaner (and it ensures a clean, declarative separation of
    concerns).
    
    I also modified the strategy for writing datapoints to the cloud to include
    the streamid for each datapoint.  This is a poorly documented feature of
    the device cloud that was added this past year.  The documentation online
    is either out of date or just very poorly written.  In testing, I hit
    an undocumented limited for which there is a workaround in software.

--- src/cwear/bridge/bridge.py
+++ src/cwear/bridge/bridge.py
@@ -1,5 +1,7 @@
 """Bridge the input from Human API into Device Cloud streams"""
 import json
+import os
+import yaml
 
 from dc import DeviceCloud, DataPoint
 from cwear.db.model import DatabaseManager
@@ -8,42 +10,7 @@ import logging
 
 logger = logging.getLogger('bridge')
 
-
-# Description of each of the batches that we handle and the keys for each
-# that we should process and use.  For all, we handle the "document" case
-# but beyond that we handle by key.
-BATCHES = {
-    "activities": {
-        "keys": {
-            "type": "string",
-            "startTime": {
-                "type": "string",
-                "units": "ISO8601"
-            },
-            "endTime": {
-                "type": "string",
-                "units": "ISO8601"
-            },
-            "duration": {
-                "type": "integer",
-                "units": "seconds",
-            },
-            "distance": {
-                "type": "integer",
-                "unit": "meters",
-            },
-            "steps": "integer",
-            "calories": "integer",
-            "timeZone": "string",
-            "sourceData": "string",
-            "timeSeries": "string",  # object
-            "updatedAt": {
-                "type": "string",
-                "units": "ISO8601"
-            },
-        }
-    }
-}
+THIS_DIR = os.path.dirname(__file__)
 
 
 class HumanAPIStreamWriter(object):
@@ -54,10 +21,10 @@ class HumanAPIStreamWriter(object):
         fullpath = "/human/{}/{}/{}".format(user_id, endpoint, path)
         self._writes.append(DataPoint(fullpath, **kwargs))
 
-    def get_writes(self):
+    def get_datapoints_written(self):
         return self._writes
 
-    def get_writes_by_path(self):
+    def get_datapoints_written_by_path(self):
         writes_by_path = {}
         for write in self._writes:
             writes_by_path.setdefault(write.path, []).append(write)
@@ -69,56 +36,82 @@ class HumanApiDeviceCloudBridge(object):
         self._db_manager = DatabaseManager()
         self._dc = DeviceCloud("connectedwearables", "Cwear12$")
         self._hapi = HumanAPI(self._db_manager)
+        self._mapping_cache = None
+
+    def get_humanapi_mapping_metadata(self):
+        if self._mapping_cache is None:
+            with open(os.path.join(THIS_DIR, "humanapi_mapping.yml"), "rb") as f:
+                self._mapping_cache = yaml.load(f)
+        return self._mapping_cache
 
     def update(self):
         """Update the bridge"""
 
         stream_writer = HumanAPIStreamWriter()
 
-        for endpoint in BATCHES:
+        for endpoint in self.get_humanapi_mapping_metadata().keys():
             data = self._hapi.get_batch(endpoint)
 
             try:
                 self.process_batch_for_endpoint(endpoint, data, stream_writer)
             except ValueError, e:
                 logger.warn(e.message)
-            else:
-                for path, writes in stream_writer.get_writes_by_path().items():
-                    self._dc.batch_stream_write(path, writes)
+
+        # after we have gathered data up for all endpoints, then batch writes out to
+        # the device cloud
+        datapoints_written = stream_writer.get_datapoints_written()
+        if len(datapoints_written) > 0:
+            self._dc.batch_stream_write(datapoints_written)
 
     def process_batch_for_endpoint(self, endpoint, batch, stream_writer):
-        endpoint_spec = BATCHES.get(endpoint)
+        endpoint_spec = self.get_humanapi_mapping_metadata().get(endpoint)
+        endpoint_timestamp_field = endpoint_spec.get("timestamp_field", "createdAt")
         for event in batch:
             datum_id = event.get("id")
             user_id = event.get("userId")
-            created_at = event.get("createdAt")  # TODO: parse ISO8601 dt
+            timestamp = event.get(endpoint_timestamp_field)  # TODO: parse ISO8601 dt?
 
             # write out full "document" to datastreams
             stream_writer.write(
                 user_id, endpoint, "document", description=datum_id,
-                unit="json", timestamp=created_at, data=json.dumps(event)
+                unit="json", timestamp=timestamp, data=json.dumps(event)
             )
 
-            for key, key_spec in endpoint_spec.get("keys").items():
-                key_tp = None
-                key_unit = "unknown"
-                value = event.get(key, None)
+            for key, key_spec in endpoint_spec.get("forward_fields").items():
+                field_type = None
+                field_unit = "unknown"
+                field_value = event.get(key, None)
+                location = None
                 if isinstance(key_spec, dict):
-                    key_tp = key_spec.get("type")
-                    key_unit = key_spec.get("unit", key_unit)
+                    field_type = key_spec.get("type")
+                    field_unit = key_spec.get("unit", field_unit)
+                    unit_field = key_spec.get("unit_field")
+                    if unit_field:
+                        field_unit = event.get(unit_field, "unknown")
                 elif isinstance(key_spec, basestring):
-                    key_tp = key_spec
+                    field_type = key_spec
                 else:
                     raise ValueError("Bad key_spec for endpoint %s" % endpoint)
 
-                if value is None:  # not included, skip it
+                # handle special field types
+                if field_type == "datetime":  # Device Cloud does not have a type for this
+                    field_type = "string"
+                    field_unit = "ISO8601"
+
+                if field_type == "boolean":
+                    field_type = "integer"
+                    field_unit = "boolean"
+                    field_value = 1 if field_value else 0
+
+                if field_value is None:  # not included, skip it
                     continue
                 else:
                     stream_writer.write(
                         user_id, endpoint, key,
-                        data_type=key_tp,
-                        unit=key_unit,
-                        data=value,
-                        timestamp=created_at,
-                        description=datum_id
+                        data_type=field_type,
+                        unit=field_unit,
+                        data=field_value,
+                        timestamp=timestamp,
+                        description=datum_id,
+                        location=location,
                     )
